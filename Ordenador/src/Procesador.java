dabd879dcf814100dd80f5d9061bcf1cf6dbddf8
Humvisa <humberto.villegas1054@gmail.com>
2025-11-27 19:46:45 +0100

    BBDD Hecha

---
+++ Ordenador/src/Procesador.java
@@ -0,0 +1,47 @@
+public class Procesador {
+    private String tipo;
+    private String velocidad;
+    private String memCache;
+    private String generacion;
+    private String nucleo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getVelocidad() {
+        return velocidad;
+    }
+
+    public void setVelocidad(String velocidad) {
+        this.velocidad = velocidad;
+    }
+
+    public String getMemCache() {
+        return memCache;
+    }
+
+    public void setMemCache(String memCache) {
+        this.memCache = memCache;
+    }
+
+    public String getGenerecaion() {
+        return generacion;
+    }
+
+    public void setGenerecaion(String generecaion) {
+        this.generacion = generecaion;
+    }
+
+    public String getNucleo() {
+        return nucleo;
+    }
+
+    public void setNucleo(String nucleo) {
+        this.nucleo = nucleo;
+    }
+}
---
+++ Ordenador/src/Torre.java
@@ -0,0 +1,29 @@
+public class Torre {
+    private String dimensiones;
+    private String puertosUSB;
+    private String tipo;
+
+    public String getDimensiones() {
+        return dimensiones;
+    }
+
+    public void setDimensiones(String dimensiones) {
+        this.dimensiones = dimensiones;
+    }
+
+    public String getPuertosUSB() {
+        return puertosUSB;
+    }
+
+    public void setPuertosUSB(String puertosUSB) {
+        this.puertosUSB = puertosUSB;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
